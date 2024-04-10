4798088e714ec1fc6410ae2e60801b727c530909
vishwa7095 <164611029+vishwa7095@users.noreply.github.com>
2024-04-10 11:20:27 +0530

    Update Controller.java

--- src/main/java/com/sathya/Rest/Restcaller/Controller.java
+++ src/main/java/com/sathya/Rest/Restcaller/Controller.java
@@ -1,14 +1,11 @@
 package com.sathya.Rest.Restcaller;
 
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.RestTemplate;
 
-import com.sathya.Rest.Entity.Hospital;
 import com.sathya.Rest.model.Quote;
 
 @RestController
